5bb265f6952573578e2dec8f53fd5379ac218464
Ryan Schmelter <rschmelter33@gmail.com>
2017-10-27 22:09:25 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,16 +1,13 @@
 
 
 def key_for_min_value(name_hash)
-  # if name_hash.empty?
-  #   nil
-  # end
+  if name_hash == {}
+    nil
+  end
   min = name_hash.first[0]
   name_hash.each do |k, v|
     if v < name_hash[min]
       min = k
-    elsif name_hash.empty?
-      nil
-
     end
   end
   min
