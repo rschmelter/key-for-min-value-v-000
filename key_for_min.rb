dd042efb1636675756ae5b25fda43de3eafa3cf7
Ryan Schmelter <rschmelter33@gmail.com>
2017-10-27 22:04:57 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,12 +1,12 @@
 
-require 'pry'
+
 def key_for_min_value(name_hash)
   if name_hash.empty?
     nil
   end
   min = name_hash.first[0]
   name_hash.each do |k, v|
-  #  binding.pry
+
     if v < name_hash[min]
       min = k
     end
