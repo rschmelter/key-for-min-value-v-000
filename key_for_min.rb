15e351ceefc0f5e03b5027223198d87a14b19645
Ryan Schmelter <rschmelter33@gmail.com>
2017-10-27 22:04:21 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,5 +1,4 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
+
 require 'pry'
 def key_for_min_value(name_hash)
   if name_hash.empty?
@@ -13,7 +12,6 @@ def key_for_min_value(name_hash)
     end
   end
   min
-
 end
 
 
